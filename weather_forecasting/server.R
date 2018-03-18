f670710f80c76598b334297b42918e127d21ade7
amrrs <amrrs@outlook.com>
2018-03-19 00:23:04 +0530

    first time series plot

--- weather_forecasting/server.R
+++ weather_forecasting/server.R
@@ -8,19 +8,96 @@
 #
 
 library(shiny)
+library(dplyr)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
+library(highcharter)
+
+require(shinydashboard)
+library(ggplot2)
+library(dplyr)
+
+recommendation <- read.csv('recommendation.csv',stringsAsFactors = F,header=T)
+
+weather <- read.csv('rainfall in india 1901-2015.csv', stringsAsFactors = F, header = T)
+
+
+# create the server functions for the dashboard  
+server <- function(input, output) { 
+  
+  #primary dropdown to select state
+  
+  output$state <- renderUI({
+    selectInput('state1','State',choices = unique(weather$SUBDIVISION),selected = 1)
+  })
+  
+  #some data manipulation to derive the values of KPI boxes
+  total.revenue <- sum(recommendation$Revenue)
+  sales.account <- recommendation %>% group_by(Account) %>% summarise(value = sum(Revenue)) %>% filter(value==max(value))
+  prof.prod <- recommendation %>% group_by(Product) %>% summarise(value = sum(Revenue)) %>% filter(value==max(value))
+  
+  
+  #creating the valueBoxOutput content
+  output$value1 <- renderValueBox({
+    valueBox(
+      formatC(sales.account$value, format="d", big.mark=',')
+      ,paste('Top Account:',sales.account$Account)
+      ,icon = icon("stats",lib='glyphicon')
+      ,color = "purple")
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
     
   })
   
-})
+  
+  
+  output$value2 <- renderValueBox({
+    
+    valueBox(
+      formatC(total.revenue, format="d", big.mark=',')
+      ,'Total Expected Revenue'
+      ,icon = icon("gbp",lib='glyphicon')
+      ,color = "green")
+    
+  })
+  
+  
+  
+  output$value3 <- renderValueBox({
+    
+    valueBox(
+      formatC(prof.prod$value, format="d", big.mark=',')
+      ,paste('Top Product:',prof.prod$Product)
+      ,icon = icon("menu-hamburger",lib='glyphicon')
+      ,color = "yellow")
+    
+  })
+  
+  #creating the plotOutput content
+  
+  output$revenuebyPrd <- renderPlot({
+    ggplot(data = recommendation, 
+           aes(x=Product, y=Revenue, fill=factor(Region))) + 
+      geom_bar(position = "dodge", stat = "identity") + ylab("Revenue (in Euros)") + 
+      xlab("Product") + theme(legend.position="bottom" 
+                              ,plot.title = element_text(size=15, face="bold")) + 
+      ggtitle("Revenue by Product") + labs(fill = "Region")
+  })
+  
+  
+  output$revenuebyRegion <- renderPlot({
+    
+    ds <- weather %>% filter(SUBDIVISION %in% input$state1) %>% group_by(SUBDIVISION,YEAR) %>% summarise(sum_of_annual = sum(ANNUAL))
+    ggplot(data = ds, aes(x=YEAR, y=sum_of_annual)) + 
+      geom_line() + ylab("Sum of Annual Rainfall") + 
+      xlab("Account") + theme(legend.position="bottom" 
+                              ,plot.title = element_text(size=15, face="bold")) + 
+      ggtitle(paste0('Rainall of ',input$state1)) 
+  })
+  
+ 
+  
+   
+  
+}
+
+
+
--- weather_forecasting/ui.R
+++ weather_forecasting/ui.R
@@ -1,33 +1,56 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(dplyr)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
+library(highcharter)
+
+require(shinydashboard)
+
+
+#Dashboard header carrying the title of the dashboard
+header <- dashboardHeader(title = "Basic Dashboard")  
+
+#Sidebar content of the dashboard
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Visit-us", icon = icon("send",lib='glyphicon'), 
+             href = "https://www.salesforce.com")
   )
-))
+)
+
+
+frow1 <- fluidRow(
+  
+  shiny::uiOutput('state'),
+  valueBoxOutput("value1")
+  ,valueBoxOutput("value2")
+  ,valueBoxOutput("value3")
+)
+
+frow2 <- fluidRow(
+  
+  box(
+    title = "Revenue per Account"
+    ,status = "primary"
+    ,solidHeader = TRUE 
+    ,collapsible = TRUE 
+    ,plotOutput("revenuebyPrd", height = "300px")
+  )
+  
+  ,box(
+    title = "Revenue per Product"
+    ,status = "primary"
+    ,solidHeader = TRUE 
+    ,collapsible = TRUE 
+    ,plotOutput("revenuebyRegion", height = "300px")
+  ) 
+  
+)
+
+
+
+# combine the two fluid rows to make the body
+body <- dashboardBody(frow1, frow2)
+
+#completing the ui part with dashboardPage
+ui <- dashboardPage(title = 'This is my Page title', header, sidebar, body, skin='red')
